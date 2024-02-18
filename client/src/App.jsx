635ce35bc0f5e9b3d42f6018b2c3e39a0fdaaa7f
Rana Usman <usman4243ch@gmail.com>
2024-02-18 13:14:46 -0800

    Complete Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Projects from './pages/Projects'
 import DashBoard from './pages/DashBoard';
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header';
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
