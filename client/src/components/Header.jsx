26168e8166b583b4612aa7ea91d4b399d96a4e23
Rana Usman <usman4243ch@gmail.com>
2024-02-18 15:13:01 -0800

    Create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                     <FaMoon />
                 </Button>
                 <Link to="/sign-in">
-                    <Button gradientDuoTone={'purpleToBlue'} pill>
+                    <Button gradientDuoTone={'purpleToBlue'} outline >
                         Sign In
                     </Button>
                 </Link>
