f59ced663de4d62d76b581757912ed0f9478d8f1
Rana Usman <usman4243ch@gmail.com>
2024-02-18 17:57:15 -0800

    Establish Mongo Connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-const app = express()
+dotenv.config()
 
-app.listen(3000, () => {
-    console.log("Server is running on 3000");
-})
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Mongo is connected');
+    })
+    .catch((error) => {
+        console.log('Error while connecting DB', error);
+    });
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error('Error while starting the server:', error);
+    } else {
+        console.log(`Server is running on port ${PORT}`);
+    }
+});
