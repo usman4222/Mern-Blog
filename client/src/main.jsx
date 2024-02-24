4f124ce8e000fa60d87beaf5d77ec1e3fabb4bb3
Rana Usman <usman4243ch@gmail.com>
2024-02-24 10:49:02 -0800

    Complete Dark Mode Functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 import { store, persister } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persister}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import themeReducer from './theme/themeSlice.js'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import persistStore from 'redux-persist/es/persistStore'
 
 //to store many reduers
 const rootRducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  theme: themeReducer
 })
 
 //to persist state in storage
