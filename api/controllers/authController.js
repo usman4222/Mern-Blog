1e7e00fad5a81f060ac11a1412e3cc2914358341
Rana Usman <usman4243ch@gmail.com>
2024-02-23 23:55:49 -0800

    create signIn API Route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/Usermodel.js"
 import bcryptjs from "bcryptjs"
 import { ErrorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -23,4 +24,38 @@ export const signUp = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
-} 
+}
+
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password || email === "" || password === "") {
+        return next(ErrorHandler(400, "Email or password is required"))
+    }
+
+    try {
+        const validUser = await User.findOne({ email })
+
+        if (!validUser) {
+            return next(ErrorHandler(404, "Invalid Email and Password"))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if (!validPassword) {
+            return next(ErrorHandler(404, "Invalid Email and Password"))
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        const { password: pass, ...rest } = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
+
+    } catch (error) {
+        next(error)
+    }
+}
