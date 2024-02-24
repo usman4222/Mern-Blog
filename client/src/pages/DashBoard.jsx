a913acbf2c2db09059d0e109fd43b84e3ef62081
Rana Usman <usman4243ch@gmail.com>
2024-02-24 11:51:03 -0800

    Create SideBar of DashBoard

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSideBar from '../components/DashSideBar'
+import DashProfile from '../components/DashProfile'
 
 const DashBoard = () => {
+
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFormUrl = urlParams.get('tab')
+    if(tabFormUrl){
+      setTab(tabFormUrl)
+    }
+  }, [location.search])
+
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+     <div className='md:w-56'>
+      <DashSideBar/>
+     </div>
+     {tab === "profile" && <DashProfile/>}
     </div>
   )
 }
