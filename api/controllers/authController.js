6f9ccb52aa6ac262a86763af32da105679237c31
Rana Usman <usman4243ch@gmail.com>
2024-02-24 23:38:19 -0800

    Add Admin Functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -47,7 +47,7 @@ export const signIn = async (req, res, next) => {
             return next(ErrorHandler(404, "Invalid Email and Password"))
         }
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET)
 
         const { password: pass, ...rest } = validUser._doc
 
@@ -69,7 +69,7 @@ export const google = async (req, res, next) => {
 
     try {
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
             const { password, ...rest } = user._doc
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
@@ -85,7 +85,7 @@ export const google = async (req, res, next) => {
                 profileImage: googlePhotoUrl
             })
             await newUser.save()
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin  }, process.env.JWT_SECRET)
             const { password, ...rest } = newUser._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
