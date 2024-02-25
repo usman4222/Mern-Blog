c019c970f579382e2ac892a0302d5319c1ef6033
Rana Usman <usman4243ch@gmail.com>
2024-02-24 22:15:14 -0800

    Delete User API Route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -33,7 +33,7 @@ export const updateUser = async (req, res, next) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
             $set: {
-                username: req.body.username, 
+                username: req.body.username,
                 email: req.body.email,
                 profileImage: req.body.profileImage,
                 password: req.body.password
@@ -41,6 +41,18 @@ export const updateUser = async (req, res, next) => {
         }, { new: true })
         const { password, ...rest } = updatedUser._doc
         res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId) {
+        return next(ErrorHandler(403, "You are not allowed to delete user"))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId)
+        res.status(200).json("User deleted successfuly")
     } catch (error) {
         next(error)
     }
