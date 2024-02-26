9991d6012818a0cb3e7c85df906f4de7c0c1c87e
Rana Usman <usman4243ch@gmail.com>
2024-02-25 18:54:47 -0800

    Add Show More Funtionality to posts result

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -9,6 +9,7 @@ const DashPosts = () => {
 
     const { currentUser } = useSelector((state) => state.user)
     const [userPosts, setUserPosts] = useState([])
+    const [showMore, setShowMore] = useState(true)
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -17,6 +18,9 @@ const DashPosts = () => {
                 const data = await res.json();
                 if (res.ok) {
                     setUserPosts(data.posts);
+                    if (data.posts.length < 9) {
+                        setShowMore(false)
+                    }
                 }
             } catch (error) {
                 console.log(error.message);
@@ -26,6 +30,22 @@ const DashPosts = () => {
             fetchPosts();
         }
     }, [currentUser._id]);
+
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+            const data = await res.json()
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts])
+                if (data.posts.length < 9) {
+                    setShowMore(false)
+                }
+            }
+        } catch (error) {
+
+        }
+    }
 
     return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -71,6 +91,9 @@ const DashPosts = () => {
                             </Table.Body>
                         ))}
                     </Table>
+                    {showMore && (
+                        <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
+                    )}
                 </>
             ) : <p>You have not post yet.</p>}
         </div>
