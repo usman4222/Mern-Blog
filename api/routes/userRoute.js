a8454d1bf2a7c73be5720b14a99aa3fd6293dc8c
Rana Usman <usman4243ch@gmail.com>
2024-02-28 21:38:41 -0800

    Create Get All Users API Route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, signOut, test, updateUser } from "../controllers/userController.js";
+import { deleteUser, getUsers, signOut, test, updateUser } from "../controllers/userController.js";
 import { verifyToken } from "../utils/authUser.js";
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.get('/', test)
 router.put('/update/:userId', verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/signout', signOut)
+router.get('/getusers', getUsers)
 
 export default router;
