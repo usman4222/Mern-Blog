7ad1970b516ec4567e4343cdf2e56960c51583bd
Rana Usman <usman4243ch@gmail.com>
2024-03-02 21:55:30 -0800

    Add recent article to post page

--- client/src/pages/PostPages.jsx
+++ client/src/pages/PostPages.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction'
 import CommentSection from '../components/CommentSection'
+import PostCard from '../components/PostCard'
 
 const PostPages = () => {
 
@@ -10,6 +11,7 @@ const PostPages = () => {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [post, setPost] = useState(null)
+    const [recentPosts, setRescentPosts] = useState(null)
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -34,6 +36,23 @@ const PostPages = () => {
         }
         fetchPost()
     }, [postSlug])
+
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`)
+                const data = await res.json()
+                if (res.ok) {
+                    setRescentPosts(data.posts)
+                }
+            }
+            fetchRecentPosts()
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [])
+
     if (loading) return (
         <div className='flex justify-center items-center '>
             <Spinner size='xl' />
@@ -59,6 +78,14 @@ const PostPages = () => {
                 <CallToAction />
             </div>
             <CommentSection postId={post?._id} />
+            <div className='flex flex-col justify-center items-center mb-5'>
+                <h1 className='text-xl mt-5'>Recent Articles</h1>
+                <div className='flex flex-wrap  gap-3 mt-5 justify-center'>
+                    {recentPosts && recentPosts.map((post) => 
+                        <PostCard key={post._id} post={post} />
+                    )}
+                </div>
+            </div>
         </main>
     )
 }
