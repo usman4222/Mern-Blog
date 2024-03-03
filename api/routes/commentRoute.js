14a1fe1fe7d15f489d783679a9c40615595e621c
Rana Usman <usman4243ch@gmail.com>
2024-03-02 23:05:54 -0800

    Add Comment API Route

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createComment, deleteComment, editComment, getPostComment, likeComment } from "../controllers/commentController.js"
+import { createComment, deleteComment, editComment, getComments, getPostComment, likeComment } from "../controllers/commentController.js"
 import { verifyToken } from "../utils/authUser.js"
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.get('/getpostcomments/:postId', getPostComment)
 router.put('/likecomment/:commentId', verifyToken, likeComment)
 router.put('/editcomment/:commentId', verifyToken, editComment)
 router.delete('/deletecomment/:commentId', verifyToken, deleteComment)
+router.get('/getcomments', verifyToken, getComments)
 
 export default router
